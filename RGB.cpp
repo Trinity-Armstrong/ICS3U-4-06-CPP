9e9ae24eec605d071f7ba3f00401986acfc4d176
Trinity-Armstrong <Trinity.Armstrong@mths.ca>
2019-11-10 04:22:46 +0000

    initial commit

---
+++ RGB.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2019 Trinity Armstrong All rights reserved.
+//
+// Created by: Trinity Armstrong
+// Created on: October 2019
+// This program outputs all the RGB colour codes
+
+#include <iostream>
+
+int main() {
+    // This function outputs all the RGB colour codes
+
+    // variables
+    int red;
+    int green;
+    int blue;
+
+    // process
+    for (red = 0; red <= 256; red++) {
+        for (green = 0; green <= 256; green++) {
+            for (blue = 0; blue <= 256; blue++) {
+                std::cout << "RGB: " << red << "," << green << "," << blue
+                << std::endl;
+            }
+        }
+    }
+}
